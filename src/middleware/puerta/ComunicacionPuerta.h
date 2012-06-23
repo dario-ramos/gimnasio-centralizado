6d78d5566572ec5b14cbdcd34ae027dde27e8116
tomaldguz@gmail.com <tomaldguz@gmail.com@388f678d-69c9-2c05-618c-4c4224571895>
2012-06-23 22:33:51 +0000

    Cambie comunicacion puerta para que no tenga herencia...

--- src/middleware/puerta/ComunicacionPuerta.h
+++ src/middleware/puerta/ComunicacionPuerta.h
@@ -1,49 +1,62 @@
 #pragma once
 #include "../common/Comunicacion.h"
 
-class ComunicacionPuerta : public Comunicacion {
+class ComunicacionPuerta {
 public:
-
+	ComunicacionPuerta(int nroPuerta);
+	virtual ~ComunicacionPuerta();
+	bool enviar_mensaje_socio(const void * msg, int msg_size);
+	bool recibir_mensaje_socio(void *, int msg_size, long msgtype);
 	bool enviar_mensaje_bus(const void * msg, int msg_size);
 
 private:
+	int envio_socio_qid;
+	int recep_socio_qid;
 	int envio_bus_qid;
 	int nroPuerta;
 
-	int inicializarComunicacion();
-	void finalizarComunicacion();
+	bool inicializarComunicacion();
 
 	key_t obtenerClaveEnvio();
 	key_t obtenerClaveRecepcion();
 
 };
 
-int ComunicacionPuerta::inicializarComunicacion(){
-	Comunicacion::inicializarComunicacion();
+ComunicacionPuerta::ComunicacionPuerta(int nroPuert) : nroPuerta(nroPuert){
+}
+
+bool ComunicacionPuerta::inicializarComunicacion(){
+	char mostrar[200];
 	key_t clave;
+
+	/*Creo la cola de recepcion*/
+	struct stat fileInfo;
+	if( !( stat(DIRECTORIO,&fileInfo) == 0 && S_ISDIR(fileInfo.st_mode) ) ){
+		UPRINTLN("Comunicacion",mostrar,"comunicacion: El directorio del ftok, %s, no existe\n", DIRECTORIO );
+		return false;
+	}
+
+	clave = ftok(DIRECTORIO, COLA_SALIDA_SISTEMA);
+	if((envio_socio_qid = msgget(clave, IPC_CREAT | IPC_EXCL | 0660)) == -1){
+		perror("inicializarComunicacion: error al crear la cola de peticiones");
+		return false;
+	}
+
+	/*Creo la cola de respuesta*/
+	clave = ftok(DIRECTORIO, COLA_ENTRADA_SISTEMA);
+	if((recep_socio_qid = msgget(clave, IPC_CREAT | IPC_EXCL | 0660)) == -1){
+		perror("inicializarComunicacion: error al crear la cola de respuesta");
+		return false;
+	}
+
+
 	/*Creo la cola de envio al bus*/
 	clave = ftok(DIRECTORIO, BASE_SALA_ENTRADA + nroPuerta);//TODO cambiar para inicializar nroPuerta
 	if((envio_bus_qid = msgget(clave, 0660)) == -1){
 		perror("inicializarComunicacion: error al crear la cola de respuesta");
-		return 1;
+		return false;
 	}
-	return 0;
-}
-
-void ComunicacionPuerta::finalizarComunicacion(){
-	Comunicacion::finalizarComunicacion();
-	/*Creo la cola de envio al bus*/
-	if(msgctl(envio_qid, IPC_RMID, (msqid_ds *) 0)){
-		perror("Comunicacion: error al borrar la cola de envio.");
-	}
-}
-
-key_t ComunicacionPuerta::obtenerClaveEnvio(){
-	return ftok(DIRECTORIO, COLA_SALIDA_SISTEMA);
-}
-
-key_t ComunicacionPuerta::obtenerClaveRecepcion(){
-	return ftok(DIRECTORIO, COLA_ENTRADA_SISTEMA);
+	return true;
 }
 
 bool ComunicacionPuerta::enviar_mensaje_bus(const void * msg, int msg_size) {
@@ -54,6 +67,34 @@ bool ComunicacionPuerta::enviar_mensaje_bus(const void * msg, int msg_size) {
 		}else{
 			char printBuffer[200];
 			UPRINTLN( "Puerta", printBuffer, "Hubo un error desconocido al registrar el socio en la sala.");
+		}
+		return false;
+	}
+	return true;
+}
+
+bool ComunicacionPuerta::enviar_mensaje_socio(const void * msg, int msg_size) {
+	if (msgsnd(envio_socio_qid, msg, msg_size - sizeof(long), 0) == -1) {
+		if(errno == EINVAL || errno == EIDRM){
+			char printBuffer[200];
+			UPRINTLN( "Puerta", printBuffer, "La cola de salida del sistema se ha destruido.");
+		}else{
+			char printBuffer[200];
+			UPRINTLN( "Puerta", printBuffer, "Hubo un error desconocido al recibir un mensaje del socio.");
+		}
+		return false;
+	}
+	return true;
+}
+
+bool ComunicacionPuerta::recibir_mensaje_socio(void *msg, int msg_size, long msgtyp){
+	if (msgrcv(recep_socio_qid, msg, msg_size - sizeof(long), msgtyp, 0)== -1) {
+		if(errno == EINVAL || errno == EIDRM){
+			char printBuffer[200];
+			UPRINTLN( "Puerta", printBuffer, "La cola de entrada del sistema se ha destruido.");
+		}else{
+			char printBuffer[200];
+			UPRINTLN( "Puerta", printBuffer, "Hubo un error desconocido al recibir mensaje del socio");
 		}
 		return false;
 	}
